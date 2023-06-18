46529bbc23adde66ae49c23e4298e397bfff74b5
Ayaan Khalif <ayaankhalif@Ayaans-MacBook-Air-4.local>
2023-06-18 16:19:59 +0300

    sum-two-smallest-numbers assignment - Done

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// Good Luck 💪🏾
+
+
+
+function sumTwoSmallestNums(nums) {
+   
+    var smallest = Infinity;
+    for (var i = 0; i < nums.length; i++) {
+      if (nums[i] < smallest) {
+        smallest = nums[i];
+      }
+    }
+  
+  
+    var secondSmallest = Infinity;
+    for (var j = 0; j < nums.length; j++) {
+      if (nums[j] < secondSmallest && nums[j] !== smallest) {
+        secondSmallest = nums[j];
+      }
+    }
+  
+   
+    var sum = smallest + secondSmallest;
+  
+    return sum;
+  }
+  
+  var numbers = [44, 21, 79, 33, 49, 56];
+  var result = sumTwoSmallestNums(numbers);
+  console.log(result);
